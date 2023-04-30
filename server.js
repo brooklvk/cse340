9a9911b3a58bdcb43b320e29d6aeb1c93658f527
brooklvk <brooklvk@byui.edu>
2023-04-29 18:56:56 -0600

    working on css, fixed server.js!

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 /* ******************************************
  * This is the application server
  * ******************************************/
-const express = require("express");
+const express = require("express")
 
-const expressLayouts = require("express-ejs-layouts");
+const expressLayouts = require("express-ejs-layouts")
 
-const app = express();
+const env = require("dotenv").config()
+
+const app = express()
 
 /* ******************************************
  * View Engine and Templates
@@ -19,21 +21,24 @@ app.set("layout", "./layouts/layout") // not at views root
  * ***************************************** */
 // app.get("/", (req, res) => {res.send("Welcome home!")});
 
+app.use(require("./routes/static"))
+
 //Index route 
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 });
 
 /* ******************************************
- * Server host name and port
+ * Server host name and 
+ Values from .env(environment) file
  * ***************************************** */
-const HOST = 'localhost';
-const PORT = 3000;
+const host = process.env.HOST
+const port = process.env.PORT
 
 /* ***********************
 * Log statement to confirm server operation
 * *********************** */
-app.listen(PORT, () => {
-console.log(`trial app listening on ${HOST}:${PORT}`)
+app.listen(port, () => {
+console.log(`trial app listening on ${host}:${port}`)
 });
 
