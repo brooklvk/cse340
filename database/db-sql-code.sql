7a7b583e935440a3ae973045a36f823f8553b992
brooklvk <brooklvk@byui.edu>
2023-05-13 10:20:45 -0600

    db edits

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -241,6 +241,19 @@ VALUES   (
     'White',
     5
   );
+  
+
+-- UPDATE inventory
+-- SET 
+--     inv_description = REPLACE (
+--         inv_description, 'the small interiors', 'a huge interior')
+-- WHERE inv_id = 10;
 
 
+UPDATE inventory
+SET 
+    inv_image = REPLACE (
+        inv_image, 'images/', 'images/vehicles/'), 
+    inv_thumbnail = REPLACE (
+        inv_thumbnail, 'images/', 'images/vehicles/');
 
