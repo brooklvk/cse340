1012365a55089a1a1f5a12ff6d539e434d553128
brooklvk <brooklvk@byui.edu>
2023-05-19 15:06:34 -0600

    fixed bug

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -16,8 +16,8 @@ async function getInventoryByClassificationId(classification_id) {
       `SELECT * FROM public.inventory AS i 
       JOIN public.classification AS c 
       ON i.classification_id = c.classification_id 
-      WHERE i.classification_id = $1,
-      [classification_id]`
+      WHERE i.classification_id = $1`,
+      [classification_id]
     )
     return data.rows
   } catch (error) {
