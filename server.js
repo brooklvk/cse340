5febd0cb658a02531fcaaf965e89ba83c13d04ad
brooklvk <brooklvk@byui.edu>
2023-05-29 12:48:28 -0600

    starting on assn 3

--- server.js
+++ server.js
@@ -3,14 +3,11 @@
  * Require statements 
  * ******************************************/
 const express = require("express")
-
 const expressLayouts = require("express-ejs-layouts")
-
 const env = require("dotenv").config()
-
 const app = express()
-
 const baseController = require("./controllers/baseController")
+const utilities = require("./utilities/")//
 
 /* ******************************************
  * View Engine and Templates
@@ -20,7 +17,7 @@ app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
 /* ******************************************
- * Default GET route
+ * Routes 
  * ***************************************** */
 // app.get("/", (req, res) => {res.send("Welcome home!")});
 
@@ -35,8 +32,24 @@ app.get("/", baseController.buildHome)
 // Inventory routes
 app.use("/inv", require("./routes/inventoryRoute"))
 
+//file not found 
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()//make utilities in scope 
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
 /* ******************************************
- * Server host name and 
+ * Local server information 
  Values from .env(environment) file
  * ***************************************** */
 const host = process.env.HOST
@@ -49,3 +62,17 @@ app.listen(port, () => {
 console.log(`trial app listening on ${host}:${port}`)
 });
 
+
+
+
+
+
+// in invController.js
+
+// const classMode1 = data1[0].inv_model
+// const classMake = data[0].inv_make 
+// res.render("filepath", {
+//   title: classMake + " " + classMode1, 
+//   nav, 
+//   grid, 
+// })
