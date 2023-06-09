300048a097d44f364d979cb0df154cc3788a9dbb
brooklvk <brooklvk@byui.edu>
2023-06-08 19:45:40 -0600

    finished unit 4 activities!!

---
+++ routes/accountRoute.js
@@ -0,0 +1,30 @@
+// Needed Resources 
+const express = require("express")
+const router = new express.Router() 
+const utilities = require("../utilities/index")
+const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
+
+// Route for account login 
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+
+// Route for registration form 
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )  
+
+// Process the login attempt
+router.post(
+    "/login",
+    (req, res) => {
+      res.status(200).send('login process')
+    }
+  )
+
+module.exports = router;
