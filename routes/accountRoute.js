1260ee453a9e6c6d893b4ad86dbb5ecc5a631cf9
brooklvk <brooklvk@byui.edu>
2023-06-09 20:28:26 -0600

    working on assn 4.

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -11,20 +11,20 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route for registration form 
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
+// Process the login attempt
+router.post(
+  "/login",
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
+
 // Process the registration data
 router.post(
     "/register",
-    regValidate.registationRules(),
-    regValidate.checkRegData,
+    regValidate.registrationRules(),
+    regValidate.checkRegData(),
     utilities.handleErrors(accountController.registerAccount)
   )  
-
-// Process the login attempt
-router.post(
-    "/login",
-    (req, res) => {
-      res.status(200).send('login process')
-    }
-  )
 
 module.exports = router;
