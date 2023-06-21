fedfa7d6cd8406784abaa460e56757668414f79c
brooklvk <brooklvk@byui.edu>
2023-06-20 19:33:48 -0600

    Revert "Revert "still no jwt/email/password check""
    This reverts commit 24bfe9ddd286e96578ab497f4a450cedea82884e.

--- server.js
+++ server.js
@@ -112,3 +112,18 @@ console.log(`app listening on ${host}:${port}`)
 // change values to value="<%- locals." so it brings in current data for update view
 //dont change til assn: -editing not accessible unless logged in as employee/admin (its ok if you can access as client!for now)
 // from account-mgmt page: welcome, links you can access (edit-inv,edit-class if management/adnim) and account updates for everyone 
+
+
+// Logins 
+// account_firstname: Basic
+// account_lastname: Client
+// account_email: basic@340.edu
+// account_password: I@mABas1cCl!3nt
+// account_firstname: Happy
+// account_lastname: Employee
+// account_email: happy@340.edu
+// account_password: I@mAnEmpl0y33
+// account_firstname: Manager
+// account_lastname: User
+// account_email: manager@340.edu
+// account_password: I@mAnAdm!n1strat0r
