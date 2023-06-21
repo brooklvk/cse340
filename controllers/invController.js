292bfabbae4835e9db14b7c62a1fa08854fb10f4
brooklvk <brooklvk@byui.edu>
2023-06-21 16:27:54 -0600

    added [0] in invcontroller

--- controllers/invController.js
+++ controllers/invController.js
@@ -157,24 +157,23 @@ invCont.buildEditInventory = async function (req, res, next) {
   let nav = await utilities.getNav()
   const itemData = await invModel.getInventoryById(inv_id)
   const options = await utilities.getClassificationOption()
-  // add [0]to all data ex. inv_make[0]
-  const itemName = `${itemData.inv_make} ${itemData.inv_model}`
-  res.render("./inventory/edit-inventory", {
+  const itemName = `${itemData[0].inv_make} ${itemData[0].inv_model}`
+  res.render("inventory/edit-inventory", {
     title: "Edit " + itemName,
     nav,
     options: options,
     errors: null,
-    inv_id: itemData.inv_id,
-    inv_make: itemData.inv_make,
-    inv_model: itemData.inv_model,
-    inv_year: itemData.inv_year,
-    inv_description: itemData.inv_description,
-    inv_image: itemData.inv_image,
-    inv_thumbnail: itemData.inv_thumbnail,
-    inv_price: itemData.inv_price,
-    inv_miles: itemData.inv_miles,
-    inv_color: itemData.inv_color,
-    classification_id: itemData.classification_id
+    inv_id: itemData[0].inv_id,
+    inv_make: itemData[0].inv_make,
+    inv_model: itemData[0].inv_model,
+    inv_year: itemData[0].inv_year,
+    inv_description: itemData[0].inv_description,
+    inv_image: itemData[0].inv_image,
+    inv_thumbnail: itemData[0].inv_thumbnail,
+    inv_price: itemData[0].inv_price,
+    inv_miles: itemData[0].inv_miles,
+    inv_color: itemData[0].inv_color,
+    classification_id: itemData[0].classification_id
   })
 }
 
